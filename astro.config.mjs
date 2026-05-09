6babce865e5458d6afa4ad82bc85f5d117482210
LucaMancusi <91390818+LucaMancusi@users.noreply.github.com>
2026-05-09 14:58:01 +0200

    chore: update dependencies and refactor layout styles
    - Updated Astro dependency from ^6.1.5 to ^6.3.1.
    - Added Sass dependency for styling.
    - Removed unused layout.css and migrated styles to layout.scss.
    - Updated Layout.astro to use new layout structure and styles.
    - Created new admin.astro page for CMS configuration.
    - Deleted old admin.html page.
    - Refactored index.astro to include new wrapper class for layout.
    - Updated global.css to import layout.scss for styling.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: "./dist",
+  trailingSlash: "ignore",
+  output: "static",
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  scopedStyleStrategy: 'where',
 });
